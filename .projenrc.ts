ae09e2d1bf197c81216802ab002dc094c7d1187a
Wojciech Wójcik <wojtaswojcik@gmail.com>
2023-03-15 08:44:22 +0100

    feat:  add auto approve feature

--- .projenrc.ts
+++ .projenrc.ts
@@ -11,17 +11,25 @@ const project = new nx_monorepo.NxMonorepoProject({
   authorEmail: "wojtaswojcik@gmail.com",
   authorName: "Wojciech Wójcik",
   github: true,
+  autoApproveUpgrades: true,
+  gitignore: [".nx/cache"],
+  autoApproveOptions: {
+    allowedUsernames: ["wwojcik", "dependabot[bot]"],
+    secret: "GITHUB_TOKEN",
+  },
   ...Recommended.defaultProjectOptions,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
-project.addGitIgnore(".nx/cache");
+
 new Recommended(project);
+
 new CodeOfConduct(project, {
   contactMethod: "email: wojtaswojcik@gmail.com",
 });
+
 new Contributors(project, {
   autoPopulateFromGit: false,
   contributors: true,
