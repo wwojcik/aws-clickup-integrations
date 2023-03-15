a6eb82b96d59ae7b6e98a019d529928feb502999
wwojcik <wwojcik@users.noreply.github.com>
2023-03-15 07:24:52 +0100

    feat: add extensions to projen (#6)

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,11 +1,13 @@
+import { CodeOfConduct, Contributors, Recommended } from "@mountainpass/cool-bits-for-projen";
 import { nx_monorepo } from "aws-prototyping-sdk";
 const project = new nx_monorepo.NxMonorepoProject({
   defaultReleaseBranch: "main",
-  devDeps: ["aws-prototyping-sdk"],
+  devDeps: ["aws-prototyping-sdk", "@mountainpass/cool-bits-for-projen"],
   name: "aws-clickup-integrations",
   authorEmail: "wojtaswojcik@gmail.com",
   authorName: "Wojciech Wójcik",
   github: true,
+  ...Recommended.defaultProjectOptions,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
@@ -13,4 +15,13 @@ const project = new nx_monorepo.NxMonorepoProject({
 });
 project.addGitIgnore(".envrc");
 project.addGitIgnore(".nx/cache");
+new Recommended(project);
+new CodeOfConduct(project, {
+  contactMethod: "email: wojtaswojcik@gmail.com",
+});
+new Contributors(project, {
+  autoPopulateFromGit: false,
+  contributors: true,
+  additionalContributors: ["Wojciech Wójcik <wojtaswojcik@gmail.com>"],
+});
 project.synth();
